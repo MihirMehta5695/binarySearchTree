566eff79b2c28cfad3402885650d3c15c86d17bb
MihirMehta5695 <mihirm5695@gmail.com>
2016-08-25 12:18:23 +0530

    Created insert(..) method which uses recursion

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,111 @@
 package binarySearchTree;
 
-public class BinarySearchTree {
+public class BinarySearchTree 
+{
 
-}
+	BstNode root,p,temp;//root is the root node,while p variable will be used to handle all primary operations of tree and p will be assisted by temp variable if required;
+	boolean isEmpty,isExisting;//flags used to indicate whether the tree exists or not and also if the tree is empty or having values;
+	
+	void createTree()
+	{
+				isEmpty=true;
+				isExisting=true;
+				root=new BstNode();
+	}//end of createTree() method
+	
+	void insert(int value)
+	{
+		
+				if(isExisting)
+				{
+							if(isEmpty)
+							{
+									root.data=value;
+									isEmpty=false;
+									return;
+							}//end of else stmt
+							
+							else
+							{
+									insert(root,value);
+									return;
+							}//end of else stmt
+					
+				}//end of if stmt
+				
+				else
+				{
+							System.out.println("The tree has yet not been created");
+				}//end of else stmt
+			
+	}//end of insert(.) method
+	
+	void insert(BstNode x,int value)
+	{
+				if(value<x.data)
+				{
+						if(x.left!=null)
+						{
+									insert(x.left,value);
+						}//end of if stmt
+						if(x.left==null)
+						{
+									temp=new BstNode(value);
+									x.left=temp;
+									isEmpty=false;
+									return;
+						}//end of if stmt
+				}//end of if stmt
+				
+				else
+				{
+						if(x.right!=null)
+						{
+									insert(x.right,value);
+						}//end of if stmt
+						if(x.right==null)
+						{
+									temp=new BstNode(value);
+									x.right=temp;
+									isEmpty=false;
+									return;
+						}//end of if stmt
+					
+				}//end of else stmt
+				
+	}//end of insert(..)method
+	
+	void delete(int value)
+	{
+				if(isExisting)
+				{
+						if(isEmpty)
+						{
+								System.out.println("List is already empty");
+						}//end of if stmt
+						
+						else
+						{
+								delete(root,value);
+						}//end of else stmt
+				}//end of if stmt
+				
+				else
+				{
+						System.out.println("Tree doesnot exists yet!");
+				}//end of else stmt
+				
+	}//end of delete(.) method
+	
+	void delete(BstNode x,int value)
+	{
+		
+	}//end of delete(..)method
+	
+	
+	
+	
+	
+	
+	
+}//end of class BinarySearchTree
